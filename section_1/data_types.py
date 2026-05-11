819e4416b4fdc825d473a24835835e1b6c3094b3
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-05-11 03:06:01 -0500

    first commit

---
+++ section_1/data_types.py
@@ -0,0 +1,11 @@
+# Section 1: Print, datatypes: integer, string, boolean.
+
+# integers 
+print (123, 912, sep= '-')
+print (20 + 10 + 7)
+
+# strings
+print ('Joao Paulo')
+print ('joao \'Paulo\'') 
+
+print (True)
