06c479fbcfa6ddc6c9eabd985125284d68e4f0fa
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-05-12 00:30:37 -0500

    first class: data types (finished)

---
+++ section_1/1.1_variables.py
@@ -0,0 +1,19 @@
+name = 'Paulo'
+year_of_birth = 2004
+age = 2026 - year_of_birth 
+height = 5.7
+age_above_18 = age > 18
+
+print ('Name',name)
+print ('Year of birth:',year_of_birth)
+print('age:',age)
+print ('Above 18?',age_above_18)
+
+
+name = "Paulo"
+age = 22
+
+
+print ('Hello my name is', name, 'I am', age, 'years old') 
+print ('Is he above 18?', age_above_18)
+
---
+++ section_1/1.2_special_operators.py
@@ -0,0 +1,15 @@
+#div 
+print (5 / 3) 
+
+#floor division
+print (5//3)
+
+# exponentiation
+print (2 ** 10)
+
+# remainder
+print (10 % 2)
+print (9 % 2)
+
+print (10 % 2 == 0 )
+print (7 % 2 == 0 )
