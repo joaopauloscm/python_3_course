de49ad59e53cea40385c0e7a6e07488ac4433f6a
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-05-12 00:47:28 -0500

    BMI Calculator added

---
+++ section_1/bmi_calculator.py
@@ -0,0 +1,9 @@
+#BMI Calculator
+
+ask_name = input("What's your name? ") 
+ask_height = 1.70
+ask_weight = 85
+
+bmi = ask_weight / (ask_height * ask_height)
+
+print (f'{ask_name} your BMI is: {bmi}' )
