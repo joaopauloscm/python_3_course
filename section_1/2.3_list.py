fd6bfe8c183b6fe228b46b8e01ae6e789454c0a2
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-06-09 14:26:49 -0300

    I learned about list, which is an ordered, mutable collection of items where I can insert, delete and update

---
+++ section_1/2.3_list.py
@@ -0,0 +1,8 @@
+random_list = [123, True, 'joao']
+print (random_list)
+random_list[2] = 'paulo'.upper()
+del random_list[0] # delete something from your list
+
+random_list.append(10) # insert something at the end of your list
+random_list.pop() # remove the last thing you added in your list or something you declared inside()
+print (random_list)
