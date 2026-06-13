7de011fdc6d797ff64e1b5597bf6163ad23cddc0
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-06-13 17:42:54 -0300

    Introduction to def/functions

---
+++ section_2/def.py
@@ -0,0 +1,21 @@
+# def
+
+def hello(): 
+    print("Hello")
+    print("Hello")
+    print("Hello")
+
+hello()
+
+def numbers(a,b,c): 
+    print(a,b,c)
+  
+
+numbers(1,2,3)
+
+def salutation(name = "No name"):
+    print (f"Hello, {name}! ")
+
+salutation('Luiz')
+salutation('Joao Paulo')
+salutation()
