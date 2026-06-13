f3da2236bc149897f3811d3cf05bdd12bb51c2c0
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-06-13 17:10:01 -0300

    added another digit for cpf_generator

--- projects/cpf_generator.py
+++ projects/cpf_generator.py
@@ -1,4 +1,5 @@
-CPF = ("956.111.020-26").replace(".","").replace("-","")
+# First digit calculus 
+CPF = ("746.824.890-70").replace(".","").replace("-","")
 CPF_size = len(CPF[0:9])
 
 i = 0
@@ -19,3 +20,29 @@ first_digit %= 11
 first_digit = 0 if first_digit > 9 else first_digit 
 
 print(f"Your first CPF digit is: {first_digit}")
+
+
+# second digit calculus 
+ten_digits = CPF[0:9] + str(first_digit)
+ten_digits_size = len(ten_digits)
+
+
+
+i = 0
+j = 11
+second_digit = 0          # <-- o acumulador nasce AQUI, fora do loop, zerado
+
+while i < ten_digits_size:
+    second_digit += int(ten_digits[i]) * j   # <-- += acumula; só a multiplicação
+    i += 1
+    j -= 1
+
+
+second_digit *= 10 
+second_digit %= 11
+
+
+
+second_digit = 0 if second_digit > 9 else second_digit
+
+print(f"Your second CPF digit is: {second_digit}")
