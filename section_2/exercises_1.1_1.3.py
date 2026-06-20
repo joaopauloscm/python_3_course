2fd5f7008a059cceed5eed7e02990674170040e4
Joao Paulo <joaopaulosantanasp@gmail.com>
2026-06-20 20:45:34 -0300

    more exercises done

---
+++ section_2/exercises_1.1_1.3.py
@@ -0,0 +1,20 @@
+def salutation(name):
+    print(f"Hello! {name}")
+
+salutation("Joao")
+
+def area(base,height):
+    return base * height 
+
+result = area(2,5)
+print (result)
+
+def exponentiation(base,exponent=2):
+    return base**exponent
+
+result_exponentiation = exponentiation(10)
+print (result_exponentiation)
+
+
+
+    
