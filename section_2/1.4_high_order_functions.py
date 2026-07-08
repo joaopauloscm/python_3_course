29c1503853b712ec3b5c60bdc3caae5d367711c3
Joao Paulo Pereira Santana <joaopaulosantanasp@gmail.com>
2026-07-08 14:19:34 -0300

    more on high order functions

---
+++ section_2/1.4_high_order_functions.py
@@ -0,0 +1,14 @@
+# def basic_sum():
+#     return 2+2
+
+# f = basic_sum
+
+# print(f())
+
+def basic_multiplication(x):
+    return x * 3
+
+def executar(funcao, valor):
+    return funcao(valor)
+
+print(executar(basic_multiplication,4))
